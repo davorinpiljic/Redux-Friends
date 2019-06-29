c0984601184438543f73af77465f6d8648764a26
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-06-29 00:18:33 -0600

    basic styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.jpg';
+import background from './background.jpg'
 import './App.css';
 import Component from './component'
 import Login from './Login'
@@ -8,9 +9,6 @@ import PrivateRoute from './PrivateRoute'
 import DeleteFriend from './DeleteFriend'
 import UpdateFriend from './UpdateFriend'
 import AddFriend from './AddFriend'
-
-
-
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
@@ -32,7 +30,8 @@ export default class App extends React.Component {
           <NavbarBrand href="/" className="mr-auto">REDUX FRIENDS</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav  navbar>
+            <Nav   navbar>
+                
                 <Link to="/">Home</Link>
                 <Link to="/protected">Friends</Link>
                 <Link to="/login">Login</Link>
--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { dataActions } from './actions'
+import logo from './logo.jpg';
+
+import { dataActions, updateItem, addFriend, friendSubmit } from './actions'
 import DeleteFriend from './DeleteFriend'
 import UpdateFriend from './UpdateFriend'
 import AddFriend from './AddFriend'
 import PrivateRoute from './PrivateRoute'
+import { Card, CardTitle, CardText } from 'reactstrap';
 
 import Loader from 'react-loader-spinner'
 import { Badge, Button, Form, FormGroup, Label, Input } from 'reactstrap'
@@ -33,21 +36,31 @@ class FriendsList extends React.Component {
         }
     return(
         <div >
+           <div className="protectednav">
         <Nav>
-          <NavItem>
-            <Link to="/protected/addfriend">Add New Friend</Link>
+
+          <NavItem className="navs">
+            <Link to="/protected/addfriend">Add</Link>
           </NavItem>
-          <NavItem>
-            <Link to="/protected/updatefriend">Update Existing Friend</Link>
+          <NavItem className="navs">
+            <Link to="/protected/updatefriend">Update</Link>
           </NavItem>
-          <NavItem>
-            <Link to="/protected/deletefriend">Delete Friend</Link>
+          <NavItem className="navs">
+            <Link to="/protected/deletefriend">Delete</Link>
           </NavItem>
 
         </Nav>
+        </div>
             <h1 className="friends-list">{this.props.friends.map((friend, key) => {
                 return (
-                    <h4 key={key}>{friend.name}</h4>
+                  <Card key={key} body inverse color="warning">
+                    <img src={logo} />
+                  <CardTitle>{`Name: ${friend.name}`}</CardTitle>
+                  <CardText>{`Age: ${friend.age}`}</CardText>
+                  <CardText>{`Height: ${friend.email}`}</CardText>
+                  {/* <Button onClick={(event) => this.changeUpdate(event, friend)} color="secondary" size="sm">Edit Friend</Button>
+                  <Button color="danger" onClick={(event) => this.deleteSmurf(event, friend)} size="sm">Delete Friend</Button> */}
+              </Card>
                 )
             })}</h1>
         </div>
@@ -63,4 +76,4 @@ const mapState = state => {
     }
 }
 
-export default connect(mapState, {dataActions})(FriendsList)
+export default connect(mapState, {dataActions, updateItem, addFriend, friendSubmit})(FriendsList)
